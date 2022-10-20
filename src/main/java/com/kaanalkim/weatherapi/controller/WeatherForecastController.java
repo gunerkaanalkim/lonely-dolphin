1457a0e2b83f7144530bf2e766420d0ad77c962c
gunerkaanalkim <g.kaanalkim@gmail.com>
2022-10-21 00:17:59 +0100

    ♻️LocalDateTime added and constructor injection via RequiredArgsConstructor

--- src/main/java/com/kaanalkim/weatherapi/controller/WeatherForecastController.java
+++ src/main/java/com/kaanalkim/weatherapi/controller/WeatherForecastController.java
@@ -3,37 +3,34 @@ package com.kaanalkim.weatherapi.controller;
 
 import com.kaanalkim.weatherapi.model.Metric;
 import com.kaanalkim.weatherapi.model.Statistic;
-import com.kaanalkim.weatherapi.model.WeatherForecast;
 import com.kaanalkim.weatherapi.payload.SensorRequest;
+import com.kaanalkim.weatherapi.payload.SensorResponse;
 import com.kaanalkim.weatherapi.service.WeatherForecastService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("weather-forecast")
 public class WeatherForecastController {
+
     private final WeatherForecastService weatherForecastService;
 
-    @Autowired
-    public WeatherForecastController(WeatherForecastService weatherForecastService) {
-        this.weatherForecastService = weatherForecastService;
-    }
-
     @PostMapping
-    public ResponseEntity<WeatherForecast> create(@RequestBody SensorRequest sensorRequest) {
-        WeatherForecast weatherForecast = this.weatherForecastService.create(sensorRequest);
-
-        return ResponseEntity.ok().body(weatherForecast);
+    public ResponseEntity<SensorResponse> create(@Valid @RequestBody SensorRequest sensorRequest) {
+        SensorResponse sensorResponse = this.weatherForecastService.create(sensorRequest);
+        return ResponseEntity.ok().body(sensorResponse);
     }
 
-    //TODO Configure datetime format in .yml file or in application level
     @GetMapping
     public ResponseEntity<Map<Metric, Double>> read(
             @RequestParam Long sensor,
@@ -42,8 +39,6 @@ public class WeatherForecastController {
             @RequestParam("from") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime startDate,
             @RequestParam("to") @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss") LocalDateTime endDate) {
         Map<Metric, Double> statistics = this.weatherForecastService.search(sensor, metrics, statistic, startDate, endDate);
-
-        return ResponseEntity.ok().body(statistics);
+        return ResponseEntity.ok(statistics);
     }
-
 }
