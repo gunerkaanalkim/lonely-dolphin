1b4d4d9070541722228746bd6104d6f3dbe342fe
gunerkaanalkim <g.kaanalkim@gmail.com>
2022-10-21 00:23:55 +0100

    🎨 method signature changing

--- src/main/java/com/kaanalkim/weatherapi/service/WeatherForecastService.java
+++ src/main/java/com/kaanalkim/weatherapi/service/WeatherForecastService.java
@@ -2,15 +2,15 @@ package com.kaanalkim.weatherapi.service;
 
 import com.kaanalkim.weatherapi.model.Metric;
 import com.kaanalkim.weatherapi.model.Statistic;
-import com.kaanalkim.weatherapi.model.WeatherForecast;
 import com.kaanalkim.weatherapi.payload.SensorRequest;
+import com.kaanalkim.weatherapi.payload.SensorResponse;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 public interface WeatherForecastService {
-    WeatherForecast create(SensorRequest sensorRequest);
+    SensorResponse create(SensorRequest sensorRequest);
 
     Map<Metric, Double> search(Long sensor, List<Metric> metric, Statistic statistic, LocalDateTime startDate, LocalDateTime endDate);
 }
