febb2a5d1bd4befbbde88a458cf0a31c37353c9a
gunerkaanalkim <g.kaanalkim@gmail.com>
2022-10-21 00:23:24 +0100

    🎨 exceptional handling and transaction mechanism

--- src/main/java/com/kaanalkim/weatherapi/service/impl/WeatherForecastServiceImpl.java
+++ src/main/java/com/kaanalkim/weatherapi/service/impl/WeatherForecastServiceImpl.java
@@ -1,46 +1,65 @@
 package com.kaanalkim.weatherapi.service.impl;
 
 
+import com.kaanalkim.weatherapi.exception.InvalidDateException;
 import com.kaanalkim.weatherapi.model.Metric;
 import com.kaanalkim.weatherapi.model.Statistic;
-import com.kaanalkim.weatherapi.model.WeatherForecast;
+import com.kaanalkim.weatherapi.model.WeatherForecastEntity;
 import com.kaanalkim.weatherapi.payload.SensorRequest;
+import com.kaanalkim.weatherapi.payload.SensorResponse;
 import com.kaanalkim.weatherapi.repository.WeatherForecastRepository;
-import com.kaanalkim.weatherapi.service.CalculationStrategy;
-import com.kaanalkim.weatherapi.service.CalculationStrategyFactory;
 import com.kaanalkim.weatherapi.service.WeatherForecastService;
+import com.kaanalkim.weatherapi.service.calculate.CalculationStrategy;
+import com.kaanalkim.weatherapi.service.calculate.CalculationStrategyFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.groupingBy;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class WeatherForecastServiceImpl implements WeatherForecastService {
     private final WeatherForecastRepository weatherForecastRepository;
     private final CalculationStrategyFactory calculationStrategyFactory;
 
+    @Transactional
     @Override
-    public WeatherForecast create(SensorRequest sensorRequest) {
-        WeatherForecast wearWeatherForecast = WeatherForecast.builder()
+    public SensorResponse create(SensorRequest sensorRequest) {
+        WeatherForecastEntity wearWeatherForecast = WeatherForecastEntity.builder()
                 .sensorId(sensorRequest.getSensorId())
                 .metric(sensorRequest.getMetric())
                 .value(sensorRequest.getValue())
                 .build();
 
-        return this.weatherForecastRepository.save(wearWeatherForecast);
+        WeatherForecastEntity save = this.weatherForecastRepository.save(wearWeatherForecast);
+
+        return SensorResponse.builder()
+                .sensorId(save.getSensorId())
+                .metric(save.getMetric())
+                .value(save.getValue())
+                .build();
     }
 
     @Override
-    public Map<Metric, Double> search(Long sensor, List<Metric> metric, Statistic statistic, LocalDateTime startDate, LocalDateTime endDate) {
-        List<WeatherForecast> weatherForecasts = this.weatherForecastRepository
-                .findBySensorIdAndMetricInAndCreatedDateBetween(sensor, metric, startDate, endDate);
+    public Map<Metric, Double> search(Long sensor, List<Metric> metrics, Statistic statistic, LocalDateTime startDate, LocalDateTime endDate) {
+        long dayDifferences = ChronoUnit.DAYS.between(startDate, endDate);
+        long monthDifferences = ChronoUnit.MONTHS.between(startDate, endDate);
 
-        Map<Metric, List<WeatherForecast>> groupedMetrics = weatherForecasts.stream().collect(groupingBy(WeatherForecast::getMetric));
+        if (dayDifferences < 1 || monthDifferences > 1 || endDate.isBefore(startDate)) {
+            throw new InvalidDateException();
+        }
+
+        List<WeatherForecastEntity> weatherForecasts = this.weatherForecastRepository
+                .findBySensorIdAndMetricInAndCreatedDateBetween(sensor, metrics, startDate, endDate);
+
+        Map<Metric, List<WeatherForecastEntity>> groupedMetrics = weatherForecasts.stream().collect(groupingBy(WeatherForecastEntity::getMetric));
 
         CalculationStrategy calculationStrategy = this.calculationStrategyFactory.findStrategyByStatistic(statistic);
         return calculationStrategy.calculate(groupedMetrics);
