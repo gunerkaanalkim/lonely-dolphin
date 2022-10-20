9af95f31d718549b659a17bb1777fa2c4f62b7b4
gunerkaanalkim <g.kaanalkim@gmail.com>
2022-10-21 00:21:41 +0100

    🎨 entity name changing

--- src/main/java/com/kaanalkim/weatherapi/repository/WeatherForecastRepository.java
+++ src/main/java/com/kaanalkim/weatherapi/repository/WeatherForecastRepository.java
@@ -1,16 +1,15 @@
 package com.kaanalkim.weatherapi.repository;
 
 import com.kaanalkim.weatherapi.model.Metric;
-import com.kaanalkim.weatherapi.model.WeatherForecast;
+import com.kaanalkim.weatherapi.model.WeatherForecastEntity;
 import com.kaanalkim.weatherapi.repository.base.BaseRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 
 @Repository
-public interface WeatherForecastRepository extends BaseRepository<WeatherForecast, Long> {
-    List<WeatherForecast> findBySensorIdAndMetricInAndCreatedDateBetween(Long sensorId, Collection<Metric> metric, LocalDateTime from, LocalDateTime to);
+public interface WeatherForecastRepository extends BaseRepository<WeatherForecastEntity, Long> {
+    List<WeatherForecastEntity> findBySensorIdAndMetricInAndCreatedDateBetween(Long sensorId, Collection<Metric> metric, LocalDateTime from, LocalDateTime to);
 }
