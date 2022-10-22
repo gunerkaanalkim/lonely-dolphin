0a4b33f382299c936b38271e90a19c7266ce85bd
gunerkaanalkim <g.kaanalkim@gmail.com>
2022-10-22 19:42:58 +0100

    ✅ unit tests for strategies

---
+++ src/test/java/com/kaanalkim/weatherapi/service/strategy/AverageCalculationStrategyTest.java
@@ -0,0 +1,42 @@
+package com.kaanalkim.weatherapi.service.strategy;
+
+import com.kaanalkim.weatherapi.model.Metric;
+import com.kaanalkim.weatherapi.model.WeatherForecastEntity;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+class AverageCalculationStrategyTest {
+    @InjectMocks
+    AverageCalculationStrategy averageCalculationStrategy;
+
+    @Test
+    void calculate() {
+        Metric metric = Metric.TEMPERATURE;
+
+        List<WeatherForecastEntity> entities = getEntities(LocalDateTime.now());
+        Map<Metric, List<WeatherForecastEntity>> metricListMap = new HashMap<>() {{
+            put(metric, entities);
+        }};
+
+        Map<Metric, Double> calculate = averageCalculationStrategy.calculate(metricListMap);
+
+        assertEquals(10.5, calculate.get(metric));
+    }
+
+    public static List<WeatherForecastEntity> getEntities(LocalDateTime now) {
+        Long sensorId = 1L;
+
+        return List.of(WeatherForecastEntity.builder().createdDate(now).sensorId(sensorId).metric(Metric.TEMPERATURE).value(10.0).build(), WeatherForecastEntity.builder().createdDate(now).sensorId(sensorId).metric(Metric.TEMPERATURE).value(11.0).build());
+    }
+
+}
